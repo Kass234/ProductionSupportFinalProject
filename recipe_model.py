dd4fdff21f4f6a36b99b07cebb7cabf45022cebb
Kass234 <samohtcoeus@gmail.com>
2026-03-24 08:57:49 -0400

    worked on recipe model

--- recipe_model.py
+++ recipe_model.py
@@ -20,4 +20,39 @@ class RecipeModel:
         self.conn.commit()
         rowid = self.cursor.lastrowid
         for ingredient in ingredients:
-            
+            sql_for_ingredient = "INSERT into ingredients(recipe_id,name,category,quantity,unit,notes) VALUES(%s,%s,%s,%s,%s,%s)"
+            self.cursor.execute(sql_for_ingredient,(rowid,ingredient["name"],ingredient["category"],ingredient["quantity"],ingredient["unit"],ingredient["notes"]))
+        self.conn.commit()
+        return {
+            "recipe_id":rowid,
+            "user_id": user_id,
+            "name": recipe_name,
+            "description": description,
+            "cooking_method":cooking_method,
+            "average_rating":average_rating,
+            "created_at":created_at,
+            "ingredients":ingredients
+        }
+    def get_all_recipes(self):
+        sql = "SELECT * FROM recipes "
+        self.cursor.execute(sql)
+        all_recipes=self.cursor.fetchall()
+        for recipes in all_recipes:
+            sql1 = "SELECT * from ingredients where recipe_id = %s"
+            self.cursor.execute(sql1,(recipes['recipe_id'],))
+            ingredients =self.cursor.fetchall()
+            recipes["ingredients"] = ingredients
+        return all_recipes
+    
+    def get_recipe(self,recipe_id):
+        sql = "SELECT * from recipes where recipe_id = %s"
+        self.cursor.execute(sql,(recipe_id,))
+        recipe_found =self.cursor.fetchone()
+        sql1 = "SELECT * from ingredients where recipe_id = %s"
+        self.cursor.execute(sql1,(recipe_found["recipe_id"],))
+        ingredients =self.cursor.fetchall()
+        recipe_found["ingredients"] = ingredients
+        return recipe_found
+    
+    def delete_recipe(self,recipe_id,user_id):
+        
